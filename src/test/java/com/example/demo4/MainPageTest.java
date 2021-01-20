69a322ff7c7df8e84f711cffefa3a40f22359293
Dav <davidvardanyan99@gmail.com>
2021-01-20 05:31:08 +0400

    Initial commit

---
+++ src/test/java/com/example/demo4/MainPageTest.java
@@ -0,0 +1,35 @@
+package com.example.demo4;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPageTest {
+    SelenideElement title = $("#search-section > div > div.row > div > h1");
+
+
+    public String  text(){
+        String text = title.getText();
+        return text;
+    }
+
+    @Test
+    public void test(){
+        open("https://staff.am/");
+        Assert.assertEquals(text(), "CAREER SEARCH STARTS HERE.");
+
+    }
+
+}
